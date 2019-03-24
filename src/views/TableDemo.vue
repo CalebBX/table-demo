6bd60065099bef58b5177075bd98fe8fdc610283
Caleb Breaux <cbrocomp@gmail.com>
2019-03-23 19:58:14 -0700

    Added table component and added routes for table demo

---
+++ src/views/TableDemo.vue
@@ -0,0 +1,15 @@
+<template>
+    <table></table>
+</template>
+
+<script>
+// @ is an alias to /src
+import Table from "@/components/Table.vue";
+
+export default {
+    name: "TableDemo",
+    components: {
+        Table
+    }
+};
+</script>
