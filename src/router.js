52a75bca73dd60d5eefc351d2398d1502138a21e
Caleb Breaux <cbrocomp@gmail.com>
2019-03-23 23:59:59 -0700

    Removed unused routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 import TableDemo from './views/TableDemo.vue'
 
 Vue.use(Router)
@@ -10,21 +9,7 @@ export default new Router({
         {
             path: '/',
             name: 'home',
-            component: Home
-        },
-        {
-            path: '/table-demo',
-            name: 'tableDemo',
             component: TableDemo
-        },
-        {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () =>
-                import(/* webpackChunkName: "about" */ './views/About.vue')
         }
     ]
 })
