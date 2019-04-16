90bf4564cbc16e3b02c2e1b5da4327b1b02cbcbf
Caleb Breaux <cbrocomp@gmail.com>
2019-04-16 15:42:09 -0700

    Updated quiz.js to the newest application

--- quiz.js
+++ quiz.js
@@ -2,6 +2,6 @@
 let fernet = require('fernet')
 let secret = new fernet.Secret('TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM=')
 // Oh no! The code is going over the edge! What are you going to do?
-let message = 'gAAAAABcltXtR69y_RZr5impboYMuhdQlvYivwmnZxV6BMuRUu7aAbUz3S30gYYUsMEFcNXTMt8S3Od9J21FSDgyv2fls_LDFnG0KA-IynAzNNYbrSZYxbuoyawkKSX7by25esxpQJe8HDKq1PYr83uS7buTDr3FOSgqA7hUcYYC2Zjjr16cpfQ0WNdfEQzdfiRUcwSAWNFO'
+let message = 'gAAAAABctlMdJ0Y2N-e1DJqXiUrEvkH3Z7RhNc3btL7YKTKS5SMd-pVd4d3G0CP-CGtKq_dXcXZuLNi0CrWbu5yZ5zMrSAcGUXUjrIX6KXa_qEDG2vqag8xlnVpxHTd4ODQyZmUqaRzBk1FB1cgRB8hvYsJt8D7CePgCrYDT39ASbwmbt6rMJhYBryXrIBeFnOMU7yTMZP-_'
 let token = new fernet.Token({ secret: secret, token: message, ttl: 0 })
 console.log(token.decode())
